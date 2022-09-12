e5516dea1a12ed8ea5e52897e98a071aeebf50ca
Milo Goodfellow <milogoodfellowworkemail@gmail.com>
2022-09-12 09:57:36 -0400

    Complete Swap raw UI
    Complete Swap raw UI
    
    Complete raw, non-functional swap user interface

--- src/views/Swap.vue
+++ src/views/Swap.vue
@@ -9,12 +9,36 @@
       <div class="column">
         <div class="from-group">
           <h2 class="swap-group-label">From</h2>
-          <div class="swap-input">
-            <input type="text" class="swap-text-in"/>
+          <div class="input-region">
+            <div class="swap-input">
+              <input type="text" class="swap-text-in"/>
+            </div>
+            <button class="swap-dropdown">
+              <img class="crypto-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Ethereum_logo_translucent.svg/640px-Ethereum_logo_translucent.svg.png"/>
+              <span class="dropdown-text">Test</span>
+              <span class="material-symbols-outlined dropdown-icon">arrow_drop_down</span>
+            </button>
           </div>
-          <button class="swap-dropdown">
-          </button>
         </div>
+        <button class="rotate-button">
+          <span class="material-symbols-outlined">swap_horiz</span>
+        </button>
+        <div class="to-group">
+          <h2 class="swap-group-label">To</h2>
+          <div class="input-region">
+            <div class="swap-input">
+              <input type="text" class="swap-text-in"/>
+            </div>
+            <button class="swap-dropdown">
+              <img class="crypto-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Ethereum_logo_translucent.svg/640px-Ethereum_logo_translucent.svg.png"/>
+              <span class="dropdown-text">Test</span>
+              <span class="material-symbols-outlined dropdown-icon">arrow_drop_down</span>
+            </button>
+          </div>
+        </div>
+        <button class="swap-button">
+          Swap
+        </button>
       </div>
     </div>
 
@@ -97,7 +121,6 @@ export default class AddWallet extends Vue {}
 }
 .header-region {
   text-align: center;
-  background-color: red;
 }
 .header {
   color: #E3E2E6;
@@ -138,18 +161,73 @@ export default class AddWallet extends Vue {}
   background-color: #1C1C1C;
   color: #E3E2E6;
   position: relative;
-  bottom: 3px;
   border-width: 1.5px;
   width: 138px;
   height: 64px;
   border-radius: 4.3px;
   border-color: #E3E2E6;
   border-style: solid;
-  left: -1px;
+  padding: 0px;
+  left: 1px;
 }
 .from-group {
   margin: auto;
-  background-color: green;
   width: 271.5px;
+}
+.to-group {
+  margin: auto;
+  position: relative;
+  top: 7px;
+  width: 271.5px;
+}
+.input-region {
+  display: flex;
+}
+.dropdown-icon {
+  font-size: 18px;
+}
+.crypto-logo {
+  width: 32.5px;
+  height: 32.5px;
+  border-radius: 100%;
+  background-color: #E3E2E6;
+  position: relative;
+  right: 6px;
+}
+.dropdown-text {
+  color: #E3E2E6;
+  font-family: "Segoe UI";
+  font-size: 17.25px;
+  position: relative;
+  bottom: 10px;
+  left: 5px;
+}
+.dropdown-icon {
+  position: relative;
+  left: 12.5px;
+  font-size: 32.5px;
+}
+.rotate-button {
+  border-radius: 100%;
+  border: none;
+  width: 42px;
+  height: 42px;
+  background-color: #B0C6FF;
+  color: #002D6E;
+  position: relative;
+  margin: auto;
+  top: 15px;
+}
+.swap-button {
+  position: relative;
+  margin: auto;
+  width: 275px;
+  height: 54px;
+  font-size: 22px;
+  color: #002D6E;
+  top: 20px;
+  border-radius: 138px;
+  background-color: #B0C6FF;
+  font-weight: bold;
 }
 </style>
