e5b9c9f82ae5242fefbe53f48fd64cff84f873fa
Milo Goodfellow <milogoodfellowworkemail@gmail.com>
2022-09-15 23:23:34 -0400

    Create swap dropdown
    Create swap dropdown
    
    Create the swap dropdown for the swap section of the wallet

--- src/views/Swap.vue
+++ src/views/Swap.vue
@@ -1,10 +1,20 @@
+/*jshint sub:true*/
 <template>
   <div class="swap">
-      <div class="row justify-center">
-        <div class="column header-region">
-          <h1 class="header">SWAP</h1>
+    <div class="row justify-center">
+      <div v-if="modalActive" class="select-modal">
+        <h1 class="select-modal-header">SELECT TOKEN</h1>
+        <div class="dropdown-option" v-for="value, key in cryptoList" :key="key" @click="changeSelect(key)">
+          <div class="option-content">
+            <img :src="value.logo" class="option-logo"/>
+            <div class="option-text">{{ value.name }}</div>
+          </div>
         </div>
       </div>
+      <div class="column header-region">
+        <h1 class="header">SWAP</h1>
+      </div>
+    </div>
     <div class="row justify-center">
       <div class="column">
         <div class="from-group">
@@ -13,7 +23,7 @@
             <div class="swap-input">
               <input type="text" class="swap-text-in" v-model="fromInfo.amt"/>
             </div>
-            <button class="swap-dropdown">
+            <button class="swap-dropdown" @click="showModal">
               <img class="crypto-logo" :src="fromInfo.logo"/>
               <span class="dropdown-text">{{ fromInfo.name }}</span>
               <span class="material-symbols-outlined dropdown-icon">arrow_drop_down</span>
@@ -29,7 +39,7 @@
             <div class="swap-input">
               <input type="text" class="swap-text-in" v-model="toInfo.amt"/>
             </div>
-            <button class="swap-dropdown">
+            <button class="swap-dropdown" @click="showModal">
               <img class="crypto-logo" :src="toInfo.logo"/>
               <span class="dropdown-text">{{ toInfo.name }}</span>
               <span class="material-symbols-outlined dropdown-icon">arrow_drop_down</span>
@@ -61,11 +71,46 @@ import { mapMutations } from 'vuex'
       amt: null,
       logo: 'test',
       name: 'test'
-    }
+    },
+    cryptoList: {
+      ETH: {
+        name: 'ETH',
+        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Ethereum_logo_translucent.svg/640px-Ethereum_logo_translucent.svg.png'
+      }
+    },
+    modalActive: 0,
+    modifyFrom: 0,
+    modifyTo: 0
   }),
   methods: {
     rotate () {
       [this.fromInfo, this.toInfo] = [this.toInfo, this.fromInfo]
+    },
+    showModal () {
+      this.modalActive = 1
+    },
+    hideModal () {
+      this.modalActive = 0
+      this.modifyFrom = 0
+      this.modifyTo = 0
+    },
+    dropFrom () {
+      this.modifyFrom = 1
+      this.showModal()
+    },
+    dropTo () {
+      this.modifyTo = 1
+      this.showModal()
+    },
+    changeSelect (whichCoin) {
+      this.hideModal()
+      if (this.modifyFrom === 1) {
+        this.fromInfo.name = this.cryptoList[whichCoin].name
+        this.fromInfo.logo = this.cryptoList[whichCoin].logo
+      } else {
+        this.toInfo.name = this.cryptoList[whichCoin].name
+        this.toInfo.logo = this.cryptoList[whichCoin].logo
+      }
     }
   }
 })
@@ -83,7 +128,7 @@ export default class AddWallet extends Vue {}
   display: flex;
   flex-direction: row;
   align-items: center;
-  margin-bottom: 27px;
+  margin-bottom: 20px;
 }
 .justify-center {
   justify-content: center;
@@ -202,5 +247,55 @@ export default class AddWallet extends Vue {}
   border-radius: 138px;
   background-color: #B0C6FF;
   font-weight: bold;
+}
+.select-modal {
+  position: absolute;
+  background-color: #272930;
+  box-shadow: 0px 2px 6px 2px rgba(0, 0, 0, 0.15), 0px 1px 2px rgba(0, 0, 0, 0.3);
+  border-radius: 16px;
+  width: 362px;
+  height: 505.98px;
+  top: 5px;
+  z-index: 100;
+}
+.select-modal-header {
+  color: #E3E2E6;
+  text-align: center;
+  font-size: 32px;
+  line-height: 32px;
+}
+.dropdown-option {
+  background-color: #1B1B1E;
+  width: 325px;
+  height: 60px;
+  border-radius: 9px;
+  margin: auto;
+  transition: 0.1s;
+  display: flex;
+  align-items: center;
+}
+.dropdown-option:hover {
+  background-color: #30333E;
+  box-shadow: 0px 8px 12px 6px rgba(0, 0, 0, 0.15), 0px 4px 4px rgba(0, 0, 0, 0.3);
+}
+.option-logo {
+  width: 32px;
+  height: 32px;
+  background-color: #E3E2E6;
+  border-radius: 100%;
+}
+.option-content {
+  margin: auto;
+  width: 290px;
+  height: 30px;
+  font-family: 'Segoe UI';
+  color: #E3E2E6;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+}
+.option-text {
+  position: relative;
+  left: 12.5px;
 }
 </style>
